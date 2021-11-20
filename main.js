e069ffd7e47ac0b0b7d2849bd40aebf519b54dcb
KazantsevBasil <50956046+KazantsevBasil@users.noreply.github.com>
2021-11-20 17:04:17 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-const uuid = _ => 'TOLKOTxxxxxx0000'.replace(/x|0/g, v => v === 'x'
-? String.fromCharCode(Math.floor(Math.random() * 26) + 97)
-: Math.floor(Math.random() * 10))
-console.log(uuid());
+
+window.onload = function() {
+	const uuid = _ => 'Яндекс0000'.replace(/x|0/g, v => v === 'x'
+	? String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+	: Math.floor(Math.random() * 10))
+	var e = document.getElementById("Promocode");
+	e.value = uuid();
+};
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("Promocode");
+
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  document.getElementById('alertId').classList.remove('hide'); 
+}
